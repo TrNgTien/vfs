461554fdd751e190b097e05124ef6de7dc9dbeb7
Tien Tran <tien.tran1@be.com.vn>
2026-03-05 15:06:11 +0700

    feat: add command

--- cmd/vfs/main.go
+++ cmd/vfs/main.go
@@ -9,6 +9,10 @@ func main() {
 	rootCmd.AddCommand(statsCmd)
 	rootCmd.AddCommand(mcpCmd)
 	rootCmd.AddCommand(dashboardCmd)
+	rootCmd.AddCommand(serveCmd)
+	rootCmd.AddCommand(statusCmd)
+	rootCmd.AddCommand(upCmd)
+	rootCmd.AddCommand(downCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
